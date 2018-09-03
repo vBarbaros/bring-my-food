9409bf768529f6ed9a1bee50197a7d5466e0d3aa
Victor Barbaros <barbarosvictor@yahoo.com>
2018-09-03 14:48:59 -0400

    fixed issues with base_url on heroku

--- bringmfapp/config.py
+++ bringmfapp/config.py
@@ -1,9 +1,9 @@
-use_on_heroku = False
+use_on_heroku = True
 use_mockDB = True
 base_url = ''
 
 
 if use_on_heroku:
-    base_url = 'https://dashboard.heroku.com/apps/bring-my-food/'
+    base_url = 'https://bring-my-food.herokuapp.com/'
 else:
     base_url = 'http://127.0.0.1:5000/'
