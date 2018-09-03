0110e1b9e38e34697b5f57d3cd4b014b00e368be
Victor Barbaros <barbarosvictor@yahoo.com>
2018-09-03 14:06:12 -0400

    solved some import issues

--- bringmfapp/__init__.py
+++ bringmfapp/__init__.py
@@ -8,3 +8,10 @@ login_manager = LoginManager(app)
 
 # Import the application views
 from bringmfapp import bring_my_food
+from bringmfapp import config
+from bringmfapp import bitlyhelper
+from bringmfapp import dbhelper
+from bringmfapp import forms
+from bringmfapp import mockdbhelper
+from bringmfapp import passwordhelper
+from bringmfapp import user
